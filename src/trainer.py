b174ea6f4d1113184e3836e32383017044c21469
aravinthakshan <aaravinth2005@gmail.com>
2025-08-18 17:06:01 +0530

    trainer in same cond as kaggle

--- src/trainer.py
+++ src/trainer.py
@@ -2,32 +2,39 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+from src.model.Aux import LatentNet
+from src.model.LatentReformer import LatentReformer
+from src.model.mnist_cnn import MNIST_CNN
+from src.utils import train_latent_autoencoder, evaluate_f1_topo_vs_reconstruction
+from dataloader import get_mnist_topo_loaders, get_advmnist_topo_loaders
+import numpy as np
 
-<<<<<<< HEAD
-def train_latent_autoencoder(latent_reformer,latent_nn,model,simple_classifier,train_loader, val_loader,  
-    epochs, lr, device,alpha=0.5, beta=2.0, gamma=1.0):
+def train_model(config):
+
+    train_loader, val_loader = get_mnist_topo_loaders("/kaggle/input/invi_mnist_64/pytorch/default/1/mnist_train_complete.npz")
+
+    bottleneck_h = 14
+    bottleneck_w = 14
+    latent_dim = 64
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    latent_reformer = LatentReformer(bottleneck_h=bottleneck_h, bottleneck_w=bottleneck_w)
+    latent_nn = LatentNet(latent_dim=latent_dim, bottleneck_h=bottleneck_h, bottleneck_w=bottleneck_w)
+
     latent_reformer.to(device)
     latent_nn.to(device)
+
+
+    model = MNIST_CNN()
+    model.load_state_dict(torch.load('/kaggle/input/classifiers/Pretrained_classifiers/mnist.pth', map_location='cpu'))
     model.to(device)
-    simple_classifier.to(device)
+    model.eval()
 
-    optimizer = torch.optim.Adam(
-        list(latent_reformer.parameters()) +
-        list(latent_nn.parameters()) +
-        list(simple_classifier.parameters()), lr=lr
-=======
-def train_model(config):
-    train(
-        epochs=config['epochs'],
-        batch_size=config['batch_size'],
-        train_dir=config['train_dir'],
-        test_dir=config['test_dir'],  
-        wandb_debug=config['wandb'], 
-        device=config['device'],
-        lr=config['lr'],
-        dataset_name=config['dataset_name'], 
-        noise_level = config['noise_level']
->>>>>>> b7cd914 (copied older repo trainer and train code)
-    )
+    latent_reformer,latent_nn,simple_classifier = train_latent_autoencoder(latent_reformer, latent_nn, model,simple_classifier, train_loader, val_loader, epochs=50, lr=1e-3, device=device)
 
-latent_reformer,latent_nn,simple_classifier = train_latent_autoencoder(latent_reformer, latent_nn, model,simple_classifier, train_loader, val_loader, epochs=50, lr=1e-3, device=device)
+    evaluate_f1_topo_vs_reconstruction(model, latent_reformer, latent_nn, val_loader, device)
+    
+    adv_loader = get_advmnist_topo_loaders("/kaggle/input/invi_mnist_64/pytorch/default/1/adversarial_mnist_cw_strong_complete.npz")
+
+    evaluate_f1_topo_vs_reconstruction(model, latent_reformer, latent_nn, adv_loader, device)
