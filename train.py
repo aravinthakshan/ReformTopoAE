df9531d92829ec03767421976b09ea8f43eea8d4
aravinthakshan <aaravinth2005@gmail.com>
2025-08-18 16:49:46 +0530

    copied older repo trainer and train code

---
+++ train.py
@@ -0,0 +1,52 @@
+from utils.trainer import train_model
+from inference import test_model
+import argparse
+import wandb
+
+def main(args):
+    config={
+        'epochs' : args.epochs,
+        'train_dir' : args.train_dir,
+        'test_dir' : args.test_dir,
+        'batch_size' : args.batch_size,
+        'device' : args.device,
+        'lr' : args.lr,
+        'wandb': args.wandbd,
+        'noise_level': args.noise_level,
+        'dataset_name':args.dataset_name,
+        'test_dataset':args.test_dataset
+    }
+    if args.wandbd:
+        wandb.login(key=args.key)
+        wandb.init(
+            project = "DeFInet",
+            config = {
+                "Epochs": args.epochs,
+                
+                "Batch Size": args.batch_size,
+                "Learning Rate": args.lr
+            }
+        )
+    
+    train_model(config)
+    test_model(config)
+    
+    
+    
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, required=False, default=8)
+    parser.add_argument('--epochs', type=int, required=False, default=10)
+    parser.add_argument('--lr', type=int, required=False, default=1e-4)
+    parser.add_argument('--device', type=str, required=False, default='cuda')
+    
+    parser.add_argument('--train_dir', type=str, required=False,default='/kaggle/input/cbsd68/CBSD68')
+    parser.add_argument('--test_dir', type=str, required=False,default='/kaggle/input/cbsd68/CBSD68')
+    parser.add_argument('--noise_level', type=int, required=False, default=25)
+    
+    parser.add_argument('--wandbd', type=bool ,default=True)
+    parser.add_argument('--dataset_name',type=str,required=False, default='Waterloo')
+    parser.add_argument('--test_dataset',type=str,required=False, default='CBSD68')
+    arguments=parser.parse_args()
+    main(arguments)
